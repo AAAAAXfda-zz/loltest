bc0d5b7cf1128b2c25137410f946e921d8b61f35
AAAAAXfda <65740278+AAAAAXfda@users.noreply.github.com>
2021-03-05 07:47:59 +0200

    Update a.lua

--- a.lua
+++ a.lua
@@ -1,7 +1,11 @@
-spawn(function()
-	if not pcall(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/AAAAAXfda/loltest/main/a.lua'))() end) then
-		return
-		else
-		    print("script")
-	end
-end)
+if
+    not pcall(
+        function()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/AAAAAXfda/loltest/main/a.lua"))()
+        end
+    )
+ then
+    return
+else
+    print("script")
+end
